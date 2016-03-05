3de4c0aad064a3f76f4513d2adf81cfdf1f526a3
Daniel <daniel.wallner@mbit.at>
2016-03-05 13:49:20 +0100

    Module Question

--- dw.bp.models/Question.fs
+++ dw.bp.models/Question.fs
@@ -1,5 +1,15 @@
 ﻿module Question
 
-type Question = class
-    
-    end
+type Question = Question of string
+type Answer = {question: Question; points: int}
+
+type Person = Person of string
+
+type SelectedList<'t> = {active: 't; rest: 't list} 
+
+type Round = 
+    |ActiveRound of SelectedList<Person> * Question
+    |FinishedRound of Person * Question * Answer list
+
+let q = Question("Ich bin mutig!")
+let a = {question=q; points=5}
